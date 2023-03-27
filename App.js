21a096f2842737183eedd274168173aa341d0ba9
Mahmud Choudhury <mahmud_uk@hotmail.com>
2023-03-27 11:28:33 +0100

    Games & Profile

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+// import Games from './tab-navigation-icons/Games';
+import UserProfile from './tab-navigation-icons/UserProfile';
 import {Ionicons} from "@expo/vector-icons"
 import NavHome from './tab-navigation-icons/NavHome';
+import React from 'react';
 
 
 export default function App() {
@@ -12,6 +15,9 @@ export default function App() {
       < Header/>
       < NavBar/>
       < NavHome/>
+      <UserProfile/>
+      {/* <Games/> */}
+
     </View>
   );
 }
---
+++ tab-navigation-icons/UserProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Ionicons} from '@expo/vector-icons';
+
+const UserProfile = () => {
+    return (
+        <Ionicons name='md-home'></Ionicons>
+    )
+  }
+  
+  export default UserProfile
