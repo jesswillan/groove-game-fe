4642d218fa359af28f6e7583bd6abd070b61dd18
Jessica Willan <j-willan@hotmail.co.uk>
2023-03-27 15:15:29 +0100

    this commit will add title to HomeScreen in app.js

--- App.js
+++ App.js
@@ -1,33 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import HomeScreen from "./screens/HomeScreen";
-import { Header } from "./components/Header";
+import {StyleSheet} from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import { Ionicons } from "@expo/vector-icons";
-
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        
-        <Stack.Screen name ='Home' component={HomeScreen}  options={{headerTitle:(props) =><Header{...props}/>}}/>
-       
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Groove Game'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
