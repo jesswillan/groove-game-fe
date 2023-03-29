abd7108d25c6a3048f0fd2529f0e44bead8b8230
Jessica Willan <j-willan@hotmail.co.uk>
2023-03-29 14:26:50 +0100

    this commit will add HomeScreen with styling, buttonTheme added to stylesheet. Colours of active and inactive tabs changed.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
-import { Text, View, Button, Image } from 'react-native';
+import {Text, View, Button, Image} from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { StyleSheet } from 'react-native';
-import {colourTheme} from '../stylesheet'
+import {StyleSheet} from 'react-native';
+import {colourTheme, buttonTheme} from '../stylesheet';
 
 //make sure to export function, not export default
 
 export const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.playlistButton}>
-      <Button color={colourTheme.white} title='Play the game'></Button>
+      <Text style={styles.welcome}>
+        Welcome to {'\n'} <Text style={styles.grooveGame}>Groove Game</Text>
+      </Text>
+      <View style={buttonTheme}>
+        <Button color={colourTheme.white} title="Play the game"></Button>
       </View>
-      <Text style={styles.instructions}>Welcome to Groove Game</Text>
+      <Text style={styles.instructions}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Cursus sit amet
+        dictum sit amet justo donec enim diam. Quisque sagittis purus sit amet
+        volutpat consequat mauris nunc congue. Eget gravida cum sociis natoque
+        penatibus et. Aenean vel elit scelerisque mauris pellentesque pulvinar
+        pellentesque habitant.
+      </Text>
     </View>
   );
 };
@@ -23,28 +33,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colourTheme.primaryColour,
-    justifyContent: 'center',
+    padding: 20,
   },
-  playlistButton: {
-    marginTop: 25,
-    backgroundColor: colourTheme.secondaryColour,
-    borderColor: colourTheme.white,
-
-    borderWidth: 2,
-    width: 250,
-    height: 50,
-    borderRadius: 20,
+  welcome: {
+    color: colourTheme.white,
     justifyContent: 'center',
     marginLeft: 'auto',
     marginRight: 'auto',
+    marginTop: 50,
+    textAlign: 'center',
+    fontSize: 26,
+  },
+  grooveGame: {
+    color: colourTheme.white,
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 50,
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
   },
   instructions: {
     color: colourTheme.white,
     justifyContent: 'center',
     marginLeft: 'auto',
     marginRight: 'auto',
-    marginTop: 20,
-  }
+    marginTop: 50,
+    textAlign: 'center',
+    fontSize: 20,
+  },
 });
 
 export default HomeScreen;
